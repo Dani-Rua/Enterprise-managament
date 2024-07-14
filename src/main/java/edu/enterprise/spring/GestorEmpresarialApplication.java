890b02ea51bb666c42001a348607326d46c42863
DaniRuaMontoya <daniel.rua@convertic.com.co>
2024-07-14 16:05:24 -0500

    Inicial commit

---
+++ src/main/java/edu/enterprise/spring/GestorEmpresarialApplication.java
@@ -0,0 +1,15 @@
+package edu.enterprise.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+public class GestorEmpresarialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestorEmpresarialApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/edu/enterprise/spring/MicroservicioDemoSpringbootDeUnGestorEmpresarialApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.enterprise.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicioDemoSpringbootDeUnGestorEmpresarialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
